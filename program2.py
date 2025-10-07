b801eb9a618665ba88634945c11a85a90eb75568
user2 <ces@Ivans-Air.lan>
2025-10-07 17:51:19 -0500

    added the second program

---
+++ program2.py
@@ -0,0 +1,5 @@
+# this new program was added given that is very important
+# for different techniques or approaches until this moment
+# how we can take deicisions without information?
+for i in range(1,10):
+	print(i)

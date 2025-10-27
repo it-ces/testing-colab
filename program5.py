9a2c266d7c79b37792a64abf2c7d3a1326d1a7cb
user1 <ces@Ivans-Air.lan>
2025-10-27 13:21:32 -0500

    add module5

---
+++ program5.py
@@ -0,0 +1,8 @@
+# we include the module 5 to track history of growth 
+# we can model the evolution in time...
+
+def history(population):
+	"""
+		this function keep the incumbent or the more powerful
+		individual in the population"""
+	pas

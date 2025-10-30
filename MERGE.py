1696e0789146b85bececa2c3a020ed12dfe321dd
user1 <ces@Ivans-Air.lan>
2025-10-30 14:28:31 -0500

    change merge.py

--- MERGE.py
+++ MERGE.py
@@ -1,6 +1,7 @@
-in this CUADERNO  we can include some WORDS that will be REPLACED
+in this notebook  we can include some WORDS that will be REPLACED
 add and extra line in MAIN
-this WORD1 is left while this WORD2 is removed from both files
+ADD THIS LINE THE SECOND TIME THAT WE USES MEREG
+this WORD1 is left while this WORD2(removed!!!) is removed from both files
 WE INCLUDE THIS LINE IN NEW BRANCH
 AND THIS LINE IN NEW BRANCH
 hey hey hey hey i need help!

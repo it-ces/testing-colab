8c6fa0bfb372c8d2b286b57137565f0d28270417
user1 <ces@Ivans-Air.lan>
2025-10-30 14:22:34 -0500

    nwer

--- MERGE.py
+++ MERGE.py
@@ -1,2 +1,3 @@
-in this notebook we can include some WORDS that will be REPLACED
+in this CUADERNO  we can include some WORDS that will be REPLACED
+add and extra line in MAIN
 this WORD1 is left while this WORD2 is removed from both files

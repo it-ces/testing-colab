c0fc1f83cec6c1ad5e41f8539895632678794af0
user1 <ces@Ivans-Air.lan>
2025-10-30 14:23:58 -0500

    change MERGE.py in BRANCH

--- MERGE.py
+++ MERGE.py
@@ -1,2 +1,4 @@
 in this notebook we can include some WORDS that will be REPLACED
 this WORD1 is left while this WORD2 is removed from both files
+WE INCLUDE THIS LINE IN NEW BRANCH
+AND THIS LINE IN NEW BRANCH

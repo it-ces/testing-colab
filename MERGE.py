ef6e8f023ca4427dc3724fa28e4613c3228d396f
user1 <ces@Ivans-Air.lan>
2025-10-30 14:27:08 -0500

    change merge

--- MERGE.py
+++ MERGE.py
@@ -3,3 +3,4 @@ add and extra line in MAIN
 this WORD1 is left while this WORD2 is removed from both files
 WE INCLUDE THIS LINE IN NEW BRANCH
 AND THIS LINE IN NEW BRANCH
+hey hey hey hey i need help!

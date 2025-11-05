f6ae24e41f6f44d573134800581f3f99af4f778d
user1 <ces@Ivans-Air.lan>
2025-11-05 16:42:47 -0500

    modify refactor and work

--- main_program.py
+++ main_program.py
@@ -2,4 +2,8 @@
 print("my pogram start here")
 this is the main program! fuck
 this is a saint theory
-
+here we can add some functionalities to this program
+given that we can develop a new project and this project involved
+a lof of new concepts and techniques,
+how we can make all this perfect or maybe interesting
+from different point of views
